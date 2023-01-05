a889991d6da0d4f03c1917b7eab5d09252223164
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-05 14:55:55 +0530

    [Bharath]-welcome msg

---
+++ Line Comparison/Line Comparison/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Line_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison computation");
+        }
+    }
+}
